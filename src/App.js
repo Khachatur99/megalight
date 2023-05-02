e1317f76ffce94a00b7c0f8e978f8e8384a80566
Xcho <x.p.00@bk.ru>
2023-05-02 14:05:08 +0400

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+    <div className="numbers">
+      
+      <a href='tel:'> 094 094 094</a>
+      <a href='tel'> 094 094 094</a>
     </div>
+    <div className="language">
+      <select >
+        <option value="en">English</option>
+        
+      </select>
+    </div>
+  </header>
+
   );
 }
 
